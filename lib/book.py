99d212ba4741f1e383d76fa69d1ea97f985865fb
mrSam <samakpan817@gmail.com>
2024-02-04 20:39:09 +0100

    Completed assigment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    author = "Sam"
+    title = "The Right Move"
+    pages = 55
+
+    def __init__(self, year):
+        self._year = year
     
-        
+    def get_year(self):
+        print("Getter function")
+        return self._year
+    
+    def set_year(self, value):
+        if not (value == "Sam" or value == "The Right Move" or value == 55):
+            raise ValueError(f"Invalid value for year: {value}")
+        else:
+            print(f"{value} is part of the book details")
+            self.author = value
+
+    year = property(get_year, set_year)
+
+s = Book("Sam")
+print(s.year)
+# print(s.pages)  # Uncommenting this line will raise an AttributeError
+s.year = 55
+print(s.year)
+c = Book(22)
+print(c.year)
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    CATEGORY = ["Nike", "Vasace", "v3", "Louis Vuitton", "Sports"]
+
+    def __init__(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if value not in Shoe.CATEGORY:
+            print(f"Error: '{value}' is not a valid category. Choose from {Shoe.CATEGORY}")
+        else:
+            self._name = value
+
+    name = property(get_name, set_name)
+
+black = Shoe("Nike")
+print(black.name)
+
+c = Shoe("Sports")
+c.name = "Louis Vuitton"
+print(c.name)
+
+
